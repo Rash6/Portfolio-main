be46bc67690a4814de5ec3b09adbd86209ed1982
Rash6 <97577733+Rash6@users.noreply.github.com>
2022-04-29 18:05:09 +0530

    done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <br></br>
+            <hr></hr>
+            <p className="footer-text">Made with ♥️ by Rashmi<br></br>
+            <br></br>
+            <small>All rights reserved © 2022 Rashmi N</small><br></br>
+            <small>Illustrations by undraw.co</small>
+            </p>
+        </div>
+    )
+}
+
+export default Footer
